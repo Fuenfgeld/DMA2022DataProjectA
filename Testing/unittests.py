1b51c99db4a4265e76ce15cc0ddf06563ab9fd51
Flatric <github@cedricjung.de>
2022-07-19 10:56:11 +0200

    Logging hinzugefügt #29

--- Testing/unittests.py
+++ Testing/unittests.py
@@ -1,14 +1,21 @@
 import sqlite3
 import unittest
 import pandas as pd
+import datetime
+import logging
 
 #Die Tests überprüfen, wie viele Einträge (Rows) in der einzulesenen CSV Datei vorhanden sind. Danach wird überprüft, wie viele Rows in der Tabelle in der Datenbank vorhanden sind. Diese beiden
 #Werte müssen identisch sein.
 #Die Faktentabelle setzt sich nur aus bestimmten Tabellen zusammen, auch hier wird überprüft wie viele Rows in der Faktentabelle vorhanden sind. Dieser Wert wird dann mit dem Wert der
 #aufaddierten Rows der genutzten Tabellen verglichen.
+
+#setup für Tests
+logging.basicConfig(filename="../Logs/log.txt", level=logging.DEBUG)
 conn = sqlite3.connect('../datawarehouse.db')
+logging.debug(" "+str(datetime.datetime.now()) + " unittests.py opened a connection to the database")
 
 class unitTestingBreastCancerClass(unittest.TestCase):
+
     def test_careplans(self):
         tablename = "careplans"
         number_of_rows_in_df, number_of_rows_in_csv = get_counts(tablename)
@@ -46,7 +53,13 @@ class unitTestingBreastCancerClass(unittest.TestCase):
         for table in list_of_tables_used_in_facts:
             sum_of_rows += get_row_count(table)  
         number_of_rows_in_df = get_row_count(tablename)
-        self.assertEqual(number_of_rows_in_df, sum_of_rows)
+        self.assertEqual(number_of_rows_in_df, sum_of_rows),
+
+    @classmethod
+    def tearDownClass(cls):
+        conn.close
+        logging.debug(" "+str(datetime.datetime.now()) + " unittests.py closed the connection to the database")
+    
 
 
 def get_counts(tablename):
@@ -66,3 +79,4 @@ def get_csv_length(tablename):
 
 if __name__ == '__main__':
     unittest.main() 
+    
